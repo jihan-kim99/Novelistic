4e52c2d712636baa1adf026693513d83b09fac3c
jihan-kim99 <jinnkenny99@gmail.com>
2024-04-12 13:30:04 +0900

    by zsh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://novelistic.site"),
@@ -26,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <body>{children}</body>
+        <body>
+          {children}
+          <Analytics />
+        </body>
       </head>
     </html>
   );
