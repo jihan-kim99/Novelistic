c17e10cc4f7fb03a126c3a9508ac2b408430c830
jihan-kim99 <jinnkenny99@gmail.com>
2024-04-12 12:00:19 +0900

    by zsh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://novelistic.site"),
   title: "노벨리스틱 | 소설 뷰어",
   description: "생동감 넘치는 소설 뷰어",
+  openGraph: {
+    title: "노벨리스틱 | 소설 뷰어",
+    description: "생동감 넘치는 소설 뷰어",
+    images: ["icon.png"],
+  },
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,19 @@
 "use client";
 
 import { useState } from "react";
-import { Box, Button, Container, IconButton, TextField, Toolbar, Typography } from "@mui/material";
-
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import {
+  Box,
+  Button,
+  Stack,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 import EPubViewer from "@/components/molecules/epubViewer";
-
-// const narou = [       
-//   <>   
-
-// <Box
-// sx={{
-//   display: "flex",
-//   overflowX: "auto",
-//   width: "100%",
-//   mt: "50px",
-//   justifyContent: "start",
-//   alignItems: "start",
-// }}
-// >
-// <Box width={{lg: '50%', xs: '100%'}}>
-//   <TextField
-//     value={webUrl}
-//     fullWidth
-//     variant="outlined"
-//     placeholder="소설가가 되자 URL 예시: https://ncode.syosetu.com/n******/1/"
-//     onChange={(e) => setWebUrl(e.target.value)}
-//     sx={{ color : "black" }}
-//   />
-// </Box>
-// </Box>
-// <Box
-// sx={{
-//   display: "flex",
-//   justifyContent: "start",
-//   alignItems: "start",
-// }}
-// >
-// <Button
-//   variant="outlined"
-//   style={{ marginTop: "50px", borderRadius: 20, width: 200, color: "black", borderColor: "black" }}
-//   onClick={handleWebUrlCrawl}
-// >
-//   Load
-// </Button>
-// </Box>
-// </>
-// ]
+import Image from "next/image";
 
 const L2i = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -76,9 +41,12 @@ const L2i = () => {
   return (
     <Box component="main" sx={{ flexGrow: 1 }}>
       <Toolbar>
-        <Typography variant="h2" fontWeight='bold' component="div">
-          Novelistic
-        </Typography>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Image src="/icon.svg" alt="logo" width={80} height={80} />
+          <Typography variant="h2" fontWeight="bold" component="div">
+            Novelistic
+          </Typography>
+        </Stack>
       </Toolbar>
       {fileText ? (
         <EPubViewer
@@ -87,7 +55,7 @@ const L2i = () => {
           setInputText={setInputText}
         />
       ) : (
-        <Box padding="0 20px 0 20px" marginBlockEnd='50px'>
+        <Box padding="0 20px 0 20px" marginBlockEnd="50px">
           <Box
             sx={{
               display: "flex",
@@ -97,50 +65,62 @@ const L2i = () => {
             }}
           >
             <Typography fontSize={40} fontWeight="bold" mt="50px">
-              택본 업로드  (pc 사용을 권장합니다)
+              택본 업로드 (pc 사용을 권장합니다)
             </Typography>
           </Box>
-        <Box display="flex" justifyContent="start" alignItems="start">
-        <Button
-          component="label"
-          role={"upload-button"}
-          variant="outlined"
-          tabIndex={-1}
-          startIcon={<CloudUploadIcon />}
-          style={{ borderRadius: 20, width: 200, marginTop: "50px", color: "black", borderColor: "black" }}
-         >
-           업로드 버튼
-           <input type="file" hidden onChange={handleFileChange} />
-         </Button>
-         </Box>
-         <Box width={{md: '70%', xs:'100%'}} sx={{mt:"50px"}}>
-         <Typography
-            fontSize={40}
-            fontWeight="bold"
-            textAlign="start"
-            marginTop='50px'
+          <Box display="flex" justifyContent="start" alignItems="start">
+            <Button
+              component="label"
+              role={"upload-button"}
+              variant="outlined"
+              tabIndex={-1}
+              startIcon={<CloudUploadIcon />}
+              style={{
+                borderRadius: 20,
+                width: 200,
+                marginTop: "50px",
+                color: "black",
+                borderColor: "black",
+              }}
             >
-            소설 텍스트로 시작
-          </Typography>
-          <TextField
-            value= {inputText}
-            fullWidth
-            variant="outlined"
-            placeholder="소설 본문을 입력해 주세요."
-            multiline
-            rows={10}
-            onChange={(e) => setInputText(e.target.value)}
-            sx={{ mt: '50px' }}
-          />
-          <Button
-            variant="outlined"
-            role="start-button"
-            startIcon={<AutoStoriesIcon />}
-            style={{ borderRadius: 20, width: 200, marginTop: "50px", color: "black", borderColor: "black" }}
-            onClick={() => setFileText(inputText)}
-          >
-            읽기 시작
-          </Button>
+              업로드 버튼
+              <input type="file" hidden onChange={handleFileChange} />
+            </Button>
+          </Box>
+          <Box width={{ md: "70%", xs: "100%" }} sx={{ mt: "50px" }}>
+            <Typography
+              fontSize={40}
+              fontWeight="bold"
+              textAlign="start"
+              marginTop="50px"
+            >
+              소설 텍스트로 시작
+            </Typography>
+            <TextField
+              value={inputText}
+              fullWidth
+              variant="outlined"
+              placeholder="소설 본문을 입력해 주세요."
+              multiline
+              rows={10}
+              onChange={(e) => setInputText(e.target.value)}
+              sx={{ mt: "50px" }}
+            />
+            <Button
+              variant="outlined"
+              role="start-button"
+              startIcon={<AutoStoriesIcon />}
+              style={{
+                borderRadius: 20,
+                width: 200,
+                marginTop: "50px",
+                color: "black",
+                borderColor: "black",
+              }}
+              onClick={() => setFileText(inputText)}
+            >
+              읽기 시작
+            </Button>
           </Box>
         </Box>
       )}
