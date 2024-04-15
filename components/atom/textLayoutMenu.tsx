c2990193b02e56f98855dcae35e2a9d64276d117
jinnkenny99 <jinnkenny99@gmail.com>
2024-04-15 13:59:34 +0900

    push from zsh

--- components/atom/textLayoutMenu.tsx
+++ components/atom/textLayoutMenu.tsx
@@ -1,8 +1,17 @@
-
 import { Button, Menu, MenuItem, Select, Typography } from '@mui/material';
 import { MouseEvent, useState } from 'react';
 
-export default function TextLayoutMenu({fontSize, setFontSize, lineSpace, setLineSpace} : {fontSize: number, setFontSize: (fontSize: number) => void, lineSpace: number, setLineSpace: (lineSpace: number) => void}) {
+export default function TextLayoutMenu({
+  fontSize,
+  setFontSize,
+  lineSpace,
+  setLineSpace,
+}: {
+  fontSize: number;
+  setFontSize: (fontSize: number) => void;
+  lineSpace: number;
+  setLineSpace: (lineSpace: number) => void;
+}) {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
@@ -14,17 +23,17 @@ export default function TextLayoutMenu({fontSize, setFontSize, lineSpace, setLin
 
   return (
     <>
-        <Button
-            id="LayoutMenu"
-            aria-controls="basic-menu"
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            variant="outlined"
-            style={{ borderRadius: 20, marginRight: 40, color: 'black', borderColor:'black'}}
-        >
-            설정
-        </Button>
+      <Button
+        id="LayoutMenu"
+        aria-controls="basic-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        variant="outlined"
+        style={{ borderRadius: 20, marginRight: 40, color: 'black', borderColor: 'black' }}
+      >
+        설정
+      </Button>
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
@@ -35,32 +44,36 @@ export default function TextLayoutMenu({fontSize, setFontSize, lineSpace, setLin
         }}
       >
         <MenuItem>
-            <Typography marginRight={2}>크기</Typography>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={fontSize}
-                onChange={(e) => setFontSize(e.target.value as number)}
-                size="small"
-            >
-                <MenuItem value={12}>12</MenuItem>
-                <MenuItem value={16}>16</MenuItem>
-                <MenuItem value={20}>20</MenuItem>
-            </Select>
+          <Typography marginRight={2}>크기</Typography>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={fontSize}
+            onChange={(e) => setFontSize(e.target.value as number)}
+            size="small"
+          >
+            <MenuItem value={12}>12</MenuItem>
+            <MenuItem value={16}>16</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={24}>24</MenuItem>
+            <MenuItem value={28}>28</MenuItem>
+          </Select>
         </MenuItem>
         <MenuItem>
-            <Typography marginRight={2}>간격</Typography>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={lineSpace}
-                onChange={(e) => setLineSpace(e.target.value as number)}
-                size="small"
-            >
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={1.5}>1.5</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-            </Select>
+          <Typography marginRight={2}>간격</Typography>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={lineSpace}
+            onChange={(e) => setLineSpace(e.target.value as number)}
+            size="small"
+          >
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={1.5}>1.5</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={2.5}>2.5</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+          </Select>
         </MenuItem>
       </Menu>
     </>
